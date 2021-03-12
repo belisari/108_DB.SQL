cb35a45f540004957dd97b5a64e320de03840692
Emil <Wota>
2021-03-12 12:38:09 +0100

    bulk - przykład

--- 00_Queries/bulk_insert.sql
+++ 00_Queries/bulk_insert.sql
@@ -1,10 +1,15 @@
 select * from pictures;
 
+
 declare @SQLstr nvarchar(1000)
-SET @SQLstr = 'BULK INSERT pictures FROM c:\Source\100_DotNety\150_DB\test_bulk01.txt'
-EXEC @SQLstr
+SET @SQLstr = 'BULK INSERT pictures FROM "c:\Source\100_DotNety\108_DB.SQL\01_BulkInsert\b01.txt"' + 
+'WITH (FIELDTERMINATOR = '','', CODEPAGE=''RAW'' )'
+print @SQLstr
+EXEC sp_executesql @SQLstr
 
 sp_who;
+
+
 
 insert into pictures (Id, Name) values ('1', 'name01');
 
