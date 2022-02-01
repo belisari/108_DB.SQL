e978715a446bd52e374b50e04758f46801c4db10
Emil <Wota>
2022-02-01 16:57:54 +0100

    dodanie projektu c#

---
+++ DatabaseTests/basics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace basics
+{
+    class Program
+    {
+
+
+        static void Main()
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new()
+                {
+                    DataSource = "<your_server.database.windows.net>",
+                    UserID = "<your_username>",
+                    Password = "<your_password>",
+                    InitialCatalog = "<your_database>"
+                };
+
+                using SqlConnection connection = new(builder.ConnectionString);
+                Console.WriteLine("\nQuery data example:");
+                Console.WriteLine("=========================================\n");
+
+                connection.Open();
+
+                String sql = "SELECT name, collation_name FROM sys.databases";
+
+                using SqlCommand command = new SqlCommand(sql, connection);
+                using SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\nDone. Press enter.");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
